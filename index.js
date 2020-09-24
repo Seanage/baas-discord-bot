0f23a79ef44469542e87c3b60bb89b478c280aa9
Seanage <own_age@msn.com>
2020-09-23 23:28:19 -0500

    the Brian commit.
    - red card for swearing or offensive language.

---
+++ index.js
@@ -0,0 +1,17 @@
+const discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new discord.Client();
+const redCardList = ['shit', 'fuck', 'bitch', 'bitching',
+  'bitches', 'fucks', 'fucking', 'fleshlight']
+
+client.on("message", async function (message) {
+  if (message.author.bot) return;
+
+  const wordsToCheck = message.content.split(' ');
+  if (wordsToCheck.find((word) => redCardList.includes(word))) {
+    await message.react("🟥");
+  }
+});
+
+client.login(config.BOT_TOKEN);
