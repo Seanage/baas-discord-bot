8e3d5429cb85725aa66b2be5015a03d68ad29ddd
Seanage <own_age@msn.com>
2020-09-24 22:15:31 -0500

    sadly adding express to keep azure app service happy.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const discord = require("discord.js");
 const config = require("./config.json");
+const express = require('express');
 
 const client = new discord.Client();
+const app = express();
+const port = 8080;
 const redCardList = ['shit', 'fuck', 'bitch', 'bitching',
   'bitches', 'fucks', 'fucking', 'fleshlight']
+
+app.get('/', (req, res) => {
+  res.send('pong!');
+});
+
+app.listen(port, () => {
+  console.log(`starting up on http://localhost:${port}`);
+})
 
 client.on("message", async function (message) {
   if (message.author.bot) return;
@@ -13,6 +24,6 @@ client.on("message", async function (message) {
     await message.react("🟥");
   }
 });
-console.log(process.env.WEBSITE_NODE_DEFAULT_VERSION);
+console.log(process.env.WEBSITE_NODE_DEFAULT_VERSION); //TODO: remove once we confirm these are getting set.
 client.login(process.env.DISCORD_BOT_TOKEN); //TODO: POC: secure this better?
 console.log('bot started');
